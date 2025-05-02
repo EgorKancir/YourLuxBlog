2375076ad4ef1ad24497f6d1b75f7e982ac94dec
EgorKancir <kancyregor@gmail.com>
2025-05-02 19:44:30 +0200

    02/05/25

--- src/js/getBlogs.js
+++ src/js/getBlogs.js
@@ -1,11 +1,14 @@
 import axios from "axios";
+import { makehtml } from "./makeHtml";
 
 export async function getBlog() {
     try {
         const { data } = await axios.get("http://localhost:3000/blogs");
-        console.log(data);
+        makehtml(data);
     } catch (error) {
         console.error(error);
         
     }
 }
+
+getBlog()
--- src/js/makeHtml.js
+++ src/js/makeHtml.js
@@ -1,5 +1,14 @@
-export function makeHtml(data) {
-    const markup = data.map(item => {
-        return ``;
-    })
+const blogList = document.querySelector(".blogList");
+export function makehtml(data){
+    const markup = data.map(item =>{
+        return `
+            <li>
+                <h1>${item.title}</h1>
+                <p>${item.author}</p>
+                <p>${item.date}</p>
+                <p>${item.article.text}</p>
+            </li>`;
+        }).join(" ");
+    blogList.innerHTML = markup;
+    return markup;
 }
